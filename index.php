dd429d885dfa9d07ff301baa59ac63b518d77acd
Shane Ross <carbonfyre@gmail.com>
2014-11-24 22:00:55 -0600

    Added some node dependencies

--- index.php
+++ index.php
@@ -3,9 +3,13 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Palace Pizza - Best Pizza in the World!</title>
+	<link rel="stylesheet" href="public/lib/bootstrap/dist/css/bootstrap.min.css">
 </head>
 <body>
 	<h1>Palace Pizza</h1>
 	<p>The best pizza in the world!!</p>
+
+	<script src="public/lib/jquery/dist/jquery.min.js"></script>
+	<script src="public/lib/bootstrap/dist/js/bootstrap.min.js"></script>
 </body>
 </html>
